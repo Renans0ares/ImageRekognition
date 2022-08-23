527a76ca7efb7a821661a00df6183797d9b7033d
Renan Soares Sousa <renan20012010@alunos.fho.edu.br>
2022-08-22 21:15:38 -0300

    Construcao do main com sidebar

--- js/main.js
+++ js/main.js
@@ -37,22 +37,22 @@ function readURL(input) {
 }
 $("#imageUpload").change(function() {
 
-  var formData = new FormData();
-  var target = 
-  formData.append("file", document.getElementById("file").files[0]);
-  var xhr = new XMLHttpRequest();
-  var eventSource = xhr.upload || xhr;
-  eventSource.addEventListener("progress", function(e){
-      var current = e.loaded || e.position ;
-      var total = e.total || e.totalSize;
-      var percant = parseInt((current/total)*100, 10);
+  // var formData = new FormData();
+  // var target = 
+  // formData.append("file", document.getElementById("file").files[0]);
+  // var xhr = new XMLHttpRequest();
+  // var eventSource = xhr.upload || xhr;
+  // eventSource.addEventListener("progress", function(e){
+  //     var current = e.loaded || e.position ;
+  //     var total = e.total || e.totalSize;
+  //     var percant = parseInt((current/total)*100, 10);
      
-  });
-  xhr.open("POST", target, true);
-  xhr.send(formData);
-  xhr.onload = function() {
-      if (this.status === 200){}
+  // });
+  // xhr.open("POST", target, true);
+  // xhr.send(formData);
+  // xhr.onload = function() {
+  //     if (this.status === 200){}
          
-  };
+  // };
     readURL(this);
 });
