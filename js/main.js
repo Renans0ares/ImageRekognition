57cc5b4055e09123e7d0220e72366da5a7ea00f4
Renan Soares Sousa <renan20012010@alunos.fho.edu.br>
2022-09-06 21:30:22 -0300

    Funcionamento da Api em JavaScript

--- js/main.js
+++ js/main.js
@@ -1,56 +1,56 @@
-(function($) {
+(function ($) {
 
 	"use strict";
 
-	var fullHeight = function() {
+	var fullHeight = function () {
 
 		$('.js-fullheight').css('height', $(window).height());
-		$(window).resize(function(){
+		$(window).resize(function () {
 			$('.js-fullheight').css('height', $(window).height());
 		});
 
 	};
 	fullHeight();
 
-	$(".toggle-password").click(function() {
+	$(".toggle-password").click(function () {
 
-	  $(this).toggleClass("fa-eye fa-eye-slash");
-	  var input = $($(this).attr("toggle"));
-	  if (input.attr("type") == "password") {
-	    input.attr("type", "text");
-	  } else {
-	    input.attr("type", "password");
-	  }
+		$(this).toggleClass("fa-eye fa-eye-slash");
+		var input = $($(this).attr("toggle"));
+		if (input.attr("type") == "password") {
+			input.attr("type", "text");
+		} else {
+			input.attr("type", "password");
+		}
 	});
 
- 
+
 })(jQuery);
 
-$(document).ready(function(){
+$(document).ready(function () {
 	$('form input').change(function () {
-	  $('form p').text(this.files.length + " Arquivo(s) Selecioando");
+		$('form p').text(this.files.length + " Arquivo(s) Selecioando");
 	});
-  });
+});
 
-function Enviar(){
+function Enviar() {
 	$.ajax({
-        url: '',
-        type:'POST',
-        dataType: 'html',
-        data: {
-            id: '1'
-        },
-        success: function(data) {
-        if ( data == 'voted' ) {
-            $('.set-result').html( 'you already voted. try again after 24 hours' );
-        } else {
-            $('.set-result').html( 'successfully voted' );
-        }
-    }
-    });
+		url: '',
+		type: 'POST',
+		dataType: 'html',
+		data: {
+			id: '1'
+		},
+		success: function (data) {
+			if (data == 'voted') {
+				$('.set-result').html('you already voted. try again after 24 hours');
+			} else {
+				$('.set-result').html('successfully voted');
+			}
+		}
+	});
 }
-function endPoint_API(){
-	window.location.href = '/Image.html'		
+function endPoint_API() {
+	window.location.href = '/Image.html'
 }
 
 
